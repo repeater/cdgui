0e49dfb64b2c154c7e32fd85d78d55eb7711a24f
Craig Godfrey <repeater@gmail.com>
2018-05-09 21:46:11 -0700

    contact form, social media, fonts, rough copy
    Signed-off-by: Craig Godfrey <repeater@gmail.com>

--- php/index.php
+++ php/index.php
@@ -7,7 +7,9 @@
     <link rel="shortcut icon" href="assets/images/cdgui.png" />
     <link rel="canonical" href="http://cdgui.com" />
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" type="text/css" media="all" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css" integrity="sha384-G0fIWCsCzJIMAVNQPfjH08cyYaUtMwjJwqiRKxxE/rx96Uroj1BtIQ6MLJuheaO9" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/cdgui.min.css" type="text/css" media="all" />
  </head>
  <body class="cdgui-home">
@@ -21,10 +23,10 @@
              <a href="#work" title="">work</a>
            </div>
            <div class="col-sm-4 col-md-3 text-sm-center d-none d-sm-block">
-             <a href="#play" title="">play</a>
+             <a href="#play" title="">downtime</a>
            </div>
            <div class="col-sm-4 col-md-3 text-sm-center d-none d-sm-block">
-             <a href="#contact" title="">contact</a>
+             <a href="#contact" title="">connect</a>
            </div>
            <div class="col-6 text-right text-sm-center text-md-left d-sm-none topnav" id="myTopnav">
              <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
@@ -42,7 +44,7 @@
           <div class="row justify-content-center">
             <div class="col-8">
               <h1>Craig Godfrey</h1>
-              <p>My money's in that office, right? If she start giving me some bullshit about it ain't there, and we got to go someplace else and get it, I'm gonna shoot you in the head then and there. Then I'm gonna shoot that bitch in the kneecaps, find out where my goddamn money is. She gonna tell me too. Hey, look at me when I'm talking to you, motherfucker. You listen: we go in there, and that nigga Winston or anybody else is in there, you the first motherfucker to get shot. You understand?</p>
+              <p>I have been developing web-based solutions for a variety of customers for quite sometime. I am thankful to have found a career path that allows me to be creative and tackle challenges along the way.</p>
             </div>
           </div>
        </div>
@@ -60,13 +62,13 @@
         </div>
         <div class="row">
           <div class="col text-center my-projects">
-            hi
+            VoxTelesys
           </div>
           <div class="col text-center my-projects">
-            you
+            Sightly
           </div>
           <div class="col text-center my-projects">
-            you
+            IO Education
           </div>
         </div>
        </div>
@@ -76,7 +78,7 @@
        <div class="col my-play">
           <div class="row">
             <div class="col-xs-12 col-md-3 text-center">
-              <h2>Away Time</h2>
+              <h2>Downtime</h2>
             </div>
            <div class="col-xs-12 col text-center text-md-left">
             <p>My money's in that office, right? If she start giving me some bullshit about it ain't there, and we got to go someplace else and get it, I'm gonna shoot you in the head then and there. Then I'm gonna shoot that bitch in the kneecaps, find out where my goddamn money is. She gonna tell me too. Hey, look at me when I'm talking to you, motherfucker. You listen: we go in there, and that nigga Winston or anybody else is in there, you the first motherfucker to get shot. You understand?</p>
@@ -89,13 +91,62 @@
        <div class="col my-contact">
           <div class="row">
             <div class="col-xs-12 col-md-3 text-center">
-              <h2>Get in Touch</h2>
+              <h2>Connect</h2>
             </div>
            <div class="col-xs-12 col text-center text-md-left">
-            <p>My money's in that office, right? If she start giving me some bullshit about it ain't there, and we got to go someplace else and get it, I'm gonna shoot you in the head then and there. Then I'm gonna shoot that bitch in the kneecaps, find out where my goddamn money is. She gonna tell me too. Hey, look at me when I'm talking to you, motherfucker. You listen: we go in there, and that nigga Winston or anybody else is in there, you the first motherfucker to get shot. You understand?</p>
+            <p>People are great. Those of you that are like-minded s well as those of you who might have the next product challenge. Regardless, shoot me an email and lets connect.
+            <p><small>(Please fill out all the fields though so I can properly respond.)</small></p>
+            <!-- Begin MailChimp Signup Form -->
+              <!-- <style type="text/css">
+              	#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+              	/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+              	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+              </style> -->
+              <div id="mc_embed_signup">
+                <form action="https://cdgui.us18.list-manage.com/subscribe/post?u=15b8d7bea7bd83d7029efa5d1&amp;id=ed6a4b544f" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                <div id="mc_embed_signup_scroll" class="row">
+
+                <div class="mc-field-group col-xs-12 col-lg-6 col-xl-4">
+                	<label for="mce-FNAME">Name
+                </label>
+                	<input type="text" value="" name="FNAME" class="required" id="mce-FNAME">
+                </div>
+                <div class="mc-field-group col-xs-12 col-lg-6 col-xl-4">
+                	<label for="mce-EMAIL">Email
+                </label>
+                	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+                </div>
+                <div class="mc-field-group col-xs-12 col-lg-6 col-xl-4">
+                	<label for="mce-PHONE">Phone
+                </label>
+                	<input type="text" name="PHONE" class="required" value="" id="mce-PHONE">
+                </div>
+                <div class="mc-field-group col-xs-12 col-lg-6 col-xl-12">
+                	<label for="mce-MMERGE2">A Brief Note
+                </label>
+                	<input type="text" value="" name="MMERGE2" class="required" id="mce-MMERGE2">
+                </div>
+                	<div id="mce-responses" class="clear">
+                		<div class="response" id="mce-error-response" style="display:none"></div>
+                		<div class="response" id="mce-success-response" style="display:none"></div>
+                	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_15b8d7bea7bd83d7029efa5d1_ed6a4b544f" tabindex="-1" value=""></div>
+                    <div class="col clear text-center"><input type="submit" value="Connect" name="connect" id="mc-embedded-subscribe" class="button"></div>
+                    </div>
+                </form>
+              </div>
+              <!--End mc_embed_signup-->
           </div>
         </div>
        </div>
+     </div>
+     <div class="social-media text-center">
+       <ul>
+         <li><a href="http://cdgui.com/downloads/craig-godfrey-resume.pdf" title="Current Resume" target="_blank"><i class="far fa-file-pdf"></i></a></li>
+         <li><a href="https://www.linkedin.com/in/craiggodfrey/" title="LinkedIn" target="_blank"><i class="fab fa-linkedin-in"></i></a></li>
+         <li><a href="https://twitter.com/cgodfrey" title="Twitter" target="_blank"><i class="fab fa-twitter"></i></a></li>
+       </ul>
+
      </div>
    </div>
    <!-- <?php echo '<p>Hello World</p>'; ?> -->
@@ -110,5 +161,6 @@
         }
     }
   </script>
+  <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[1]='FNAME';ftypes[1]='text';fnames[0]='EMAIL';ftypes[0]='email';fnames[4]='PHONE';ftypes[4]='phone';fnames[2]='MMERGE2';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
  </body>
 </html>
